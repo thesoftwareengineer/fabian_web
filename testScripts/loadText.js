d6a14ed6b05ff53c47bf4a53495fe6064b0501cc
Eli <egonzalezjr555@gmail.com>
2016-09-11 03:33:39 -0700

    Final Commit for the weekend

--- testScripts/loadText.js
+++ testScripts/loadText.js
@@ -2,22 +2,19 @@ $(document).ready(function () {
     var file_to_get = ""
     $.get("Scripts/", function (data) {
         var file_list = $(data).find('#files').text();
-        file_list = file_list.split('\n').sort()
-        console.log('Scripts/' + file_list[file_list.length - 2]);
-        var file = 'Scripts/' + file_list[file_list.length - 2];
+        file_list = file_list.split('\n').sort();
         $.ajax({
-            url: file,
+            url: 'Scripts/' + file_list[1],
             success: function (result) {
-                result_id = $(result).filter('#body-section');
+                result_id = $(result).find('#body-section');
                 console.log(result_id.text())
                 $("#posts").append('<p>' + result_id.text() + '</p>');
             }
         });
-        file = 'Scripts/' + file_list[file_list.length - 3];
         $.ajax({
-            url: file,
+            url: 'Scripts/' + file_list[3],
             success: function (result) {
-                result_id = $(result).filter('#body-section');
+                result_id = $(result).find('#body-section');
                 console.log(result_id.text())
                 $("#posts").append('<a>' + result_id.text() + '</a>');
             }
