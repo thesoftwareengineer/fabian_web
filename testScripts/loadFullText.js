658d90cb6cfda73af132834bb12f145c36675d9c
Eli <egonzalezjr555@gmail.com>
2016-09-11 14:26:16 -0700

    Blog loading works

--- testScripts/loadFullText.js
+++ testScripts/loadFullText.js
@@ -5,13 +5,17 @@ $(document).ready(function () {
     $.get("Scripts/", function (data) {
         var file_list = $(data).find('#files').text();
         file_list = file_list.split('\n').sort();
-        var i = file_list.length - 1;
+        var i = getCookie("i_value");
+        if (i == "") {
+            i = file_list.length - 1;
+        }
         console.log(file_list[i]);
         reload(i, file_list);
         setCookie("i_value", i, 1);
         $('#newer').click(function () {
             i++;
             if (i <= file_list.length) {
+                setCookie("i_value", i, 1);
                 reload(i, file_list);
             } else {
                 alert('This is already the latest post!');
@@ -20,6 +24,7 @@ $(document).ready(function () {
         $('#older').click(function () {
             i--;
             if (i >= 1) {
+                setCookie("i_value", i, 1);
                 reload(i, file_list);
             } else {
                 alert('This is already the oldest post!');
