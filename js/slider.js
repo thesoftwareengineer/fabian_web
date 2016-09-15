15a4468ef99c7cc25d6b55ce3baa4b1e75db2261
Eli <egonzalezjr555@gmail.com>
2016-09-15 12:45:21 -0700

    Creating image slider from tutorial

--- js/slider.js
+++ js/slider.js
@@ -4,6 +4,16 @@ $(document).ready(function () {
     var slide_count = ul.children().length;
     var slide_width_pc = 100.0 / slide_count;
     var slide_index = 0;
+    var first_slide = ul.find("li:first-child");
+    var last_slide = ul.find("li:last-child");
+
+    // Clone the last slide and add as first li element
+    last_slide.clone().prependTo(ul);
+
+    // Clone the first slide and add as last li element
+    first_slide.clone().appendTo(ul);
+
+    ul.css("margin-left", "-100%");
 
     ul.find("li").each(function (indx) {
         var left_percent = (slide_width_pc * indx) + "%";
@@ -29,13 +39,22 @@ $(document).ready(function () {
 
     function slide(new_slide_index) {
 
-        if (new_slide_index < 0 || new_slide_index >= slide_count) return;
-
-        var margin_left_pc = (new_slide_index * (-100)) + "%";
+        var margin_left_pc = (new_slide_index * (-100) - 100) + "%";
 
         ul.animate({
             "margin-left": margin_left_pc
         }, 400, function () {
+
+            // If new slide is before first slide...
+            if (new_slide_index < 0) {
+                ul.css("margin-left", ((slide_count) * (-100)) + "%");
+                new_slide_index = slide_count - 1;
+            }
+            // If new slide is after last slide...
+            else if (new_slide_index >= slide_count) {
+                ul.css("margin-left", "-100%");
+                new_slide_index = 0;
+            }
 
             slide_index = new_slide_index
 
